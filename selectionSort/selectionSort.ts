e8c4f90ec2a9522a029b4c3b8994e00310b76fea
LeThanh <kzxlmdtn888@gmail.com>
2022-05-11 17:28:20 +0700

    ..

---
+++ selectionSort/selectionSort.ts
@@ -0,0 +1,31 @@
+function selectionSort(arr){
+    let i, j, min_idx;
+
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < arr.length-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        for (j = i + 1; j < arr.length; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+
+        // Swap the found minimum element with the first element
+        let temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+function printArray( arr, size)
+{
+    let i;
+    for (i = 0; i < size; i++)
+        console.log(arr[i] + " ");
+    console.log("");
+}
+
+let arr = [64, 25, 12, 22, 11];
+selectionSort(arr);
+console.log("Sorted array:");
+printArray(arr, arr.length);
